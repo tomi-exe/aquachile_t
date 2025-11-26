eb7647331a3f90c97c6dc90e109656a690bcc05c
tomi-exe <1tomas.hernandez.p@gmail.com>
2025-11-26 20:25:36 -0300

    Update urls.py

--- aquachile/urls.py
+++ aquachile/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/simulation/')),
     path('admin/', admin.site.urls),
     path('simulation/', include('simulation.urls')),
 ]
